7ec0411ec86688b1ddeddd434d68be1ab41279aa
Dung Ta Van <itprono3@gmail.com>
2019-12-08 22:29:37 +0700

    runnable

---
+++ ezyhttp-server-core/src/main/java/com/tvd12/ezyhttp/server/core/util/DoGetAnnotations.java
@@ -0,0 +1,27 @@
+package com.tvd12.ezyhttp.server.core.util;
+
+import com.tvd12.ezyfox.io.EzyStrings;
+import com.tvd12.ezyhttp.server.core.annotation.DoGet;
+import com.tvd12.ezyhttp.server.core.constant.Constants;
+import com.tvd12.ezyhttp.server.core.constant.ContentTypes;
+
+public final class DoGetAnnotations {
+
+	private DoGetAnnotations() {}
+	
+	public static String getURI(DoGet annotation) {
+		String uri = annotation.value();
+		if(EzyStrings.isNoContent(uri))
+			uri = annotation.uri();
+		if(EzyStrings.isNoContent(uri))
+			uri = Constants.EMPTY_STRING;
+		return uri;
+	}
+	
+	public static String getResponseType(DoGet annotation) {
+		String responseType = annotation.responseType();
+		if(EzyStrings.isNoContent(responseType))
+			responseType = ContentTypes.APPLICATION_JSON;
+		return responseType;
+	}
+}
