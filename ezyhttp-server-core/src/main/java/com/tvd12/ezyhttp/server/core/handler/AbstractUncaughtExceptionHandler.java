ed8de57ddc758b81c0acd8eae7808e665e902d24
Dung Ta Van <itprono3@gmail.com>
2020-11-01 22:42:26 +0700

    update exception handler

--- ezyhttp-server-core/src/main/java/com/tvd12/ezyhttp/server/core/handler/AbstractUncaughtExceptionHandler.java
+++
@@ -1,8 +0,0 @@
-package com.tvd12.ezyhttp.server.core.handler;
-
-import com.tvd12.ezyfox.util.EzyLoggable;
-
-public abstract class AbstractUncaughtExceptionHandler
-		extends EzyLoggable
-		implements UncaughtExceptionHandler {
-}
