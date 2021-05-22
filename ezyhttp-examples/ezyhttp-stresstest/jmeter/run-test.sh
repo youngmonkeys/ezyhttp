2fb7f5d336275877f6adb30a8b9d0afe59ac20df
Dung Ta Van <itprono3@gmail.com>
2021-05-22 14:43:32 +0700

    remove examples

--- ezyhttp-examples/ezyhttp-stresstest/jmeter/run-test.sh
+++
@@ -1,5 +0,0 @@
-# config: 
-# - jmeter.properties: remote_hosts=127.0.0.1
-# - user.properties: server.rmi.ssl.disable=true
-jmeter -Djava.rmi.server.hostname=127.0.0.1 -s
-jmeter -n --testfile test.jmx -l result.jtl -r
