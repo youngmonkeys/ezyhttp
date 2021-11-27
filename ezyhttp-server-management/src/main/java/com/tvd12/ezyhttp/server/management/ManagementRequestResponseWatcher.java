3d4ecca9f62b41cec6dab437bbacdfbe6948c0f2
Dung Ta Van <itprono3@gmail.com>
2021-11-27 21:30:44 +0700

    update management

--- ezyhttp-server-management/src/main/java/com/tvd12/ezyhttp/server/management/ManagementRequestResponseWatcher.java
+++ ezyhttp-server-management/src/main/java/com/tvd12/ezyhttp/server/management/ManagementRequestResponseWatcher.java
@@ -3,20 +3,29 @@ package com.tvd12.ezyhttp.server.management;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
+import com.tvd12.ezyfox.annotation.EzyProperty;
 import com.tvd12.ezyfox.bean.annotation.EzySingleton;
 import com.tvd12.ezyhttp.core.constant.HttpMethod;
 import com.tvd12.ezyhttp.server.core.handler.RequestResponseWatcher;
 import com.tvd12.ezyhttp.server.management.monitor.SystemMonitor;
 
+import lombok.Setter;
+
+@Setter
 @EzySingleton
 public class ManagementRequestResponseWatcher implements RequestResponseWatcher {
 
+    @EzyProperty("management.port")
+    protected int managementPort;
+    
     @Override
     public void watchRequest(
             HttpMethod method, 
             ServletRequest request
     ) {
-        SystemMonitor.getInstance().increaseRequestCount();
+        if (request.getServerPort() != managementPort) {
+            SystemMonitor.getInstance().increaseRequestCount();
+        }
     }
     
     @Override
@@ -25,6 +34,8 @@ public class ManagementRequestResponseWatcher implements RequestResponseWatcher 
             ServletRequest request, 
             ServletResponse response
     ) {
-        SystemMonitor.getInstance().increaseResponseCount();
+        if (request.getServerPort() != managementPort) {
+            SystemMonitor.getInstance().increaseResponseCount();
+        }
     }
 }
