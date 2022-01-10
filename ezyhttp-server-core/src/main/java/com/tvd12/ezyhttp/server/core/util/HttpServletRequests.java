c5c97be6dbbb63cea2ff21b317dfa7710ef98d32
Dung Ta Van <itprono3@gmail.com>
2022-01-10 20:01:16 +0700

    add HttpServletRequests

---
+++ ezyhttp-server-core/src/main/java/com/tvd12/ezyhttp/server/core/util/HttpServletRequests.java
@@ -0,0 +1,32 @@
+package com.tvd12.ezyhttp.server.core.util;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import static com.tvd12.ezyfox.io.EzyStrings.isNotBlank;
+
+public final class HttpServletRequests {
+    
+    private HttpServletRequests() {}
+    
+    public static String getRequestValue(HttpServletRequest request, String name) {
+        String value = (String)request.getAttribute(name);
+        if (value == null) {
+            value = request.getHeader(name);
+        }
+        if (value == null) {
+            value = request.getParameter(name);
+        }
+        if (value == null && request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if (cookie.getName().equals(name)) {
+                    value = cookie.getValue();
+                    if (isNotBlank(value)) {
+                        break;
+                    }
+                }
+            }
+        }
+        return value;
+    }
+}
---
+++ ezyhttp-server-core/src/test/java/com/tvd12/ezyhttp/server/core/test/util/HttpServletRequestsTest.java
@@ -0,0 +1,67 @@
+package com.tvd12.ezyhttp.server.core.test.util;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import org.testng.annotations.Test;
+
+import com.tvd12.ezyhttp.server.core.util.HttpServletRequests;
+import com.tvd12.test.assertion.Asserts;
+import com.tvd12.test.base.BaseTest;
+
+public class HttpServletRequestsTest extends BaseTest {
+    
+    @Override
+    public Class<?> getTestClass() {
+        return HttpServletRequests.class;
+    }
+    
+    @Test
+    public void getRequestValueTest() {
+        // given
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        
+        // when
+        when(request.getAttribute("1")).thenReturn("a");
+        when(request.getHeader("2")).thenReturn("b");
+        when(request.getParameter("3")).thenReturn("c");
+        
+        // then
+        Asserts.assertEquals(HttpServletRequests.getRequestValue(request, "1"), "a");
+        Asserts.assertEquals(HttpServletRequests.getRequestValue(request, "2"), "b");
+        Asserts.assertEquals(HttpServletRequests.getRequestValue(request, "3"), "c");
+        Asserts.assertNull(HttpServletRequests.getRequestValue(request, "unknown"));
+        
+        verify(request, times(1)).getAttribute("1");
+        verify(request, times(1)).getHeader("2");
+        verify(request, times(1)).getParameter("3");
+        verify(request, times(1)).getCookies();
+    }
+    
+    @Test
+    public void getRequestValueTestWithCookie() {
+        // given
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        
+        // when
+        when(request.getCookies()).thenReturn(new Cookie[] {
+           new Cookie("1", ""), new Cookie("2", "b"), new Cookie("3", "c") 
+        });
+        
+        // then
+        Asserts.assertEquals(HttpServletRequests.getRequestValue(request, "1"), "");
+        Asserts.assertEquals(HttpServletRequests.getRequestValue(request, "2"), "b");
+        Asserts.assertEquals(HttpServletRequests.getRequestValue(request, "3"), "c");
+        Asserts.assertNull(HttpServletRequests.getRequestValue(request, "unknown"));
+        
+        verify(request, times(1)).getAttribute("1");
+        verify(request, times(1)).getHeader("2");
+        verify(request, times(1)).getParameter("3");
+        verify(request, times(8)).getCookies();
+    }
+}
