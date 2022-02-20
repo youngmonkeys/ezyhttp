a75244f561ec25caeeaf4ba467c24e7568898d86
Dung Ta Van <itprono3@gmail.com>
2022-02-20 13:02:12 +0700

    use common EzyActionFrame

--- ezyhttp-server-management/src/main/java/com/tvd12/ezyhttp/server/management/monitor/SystemMonitor.java
+++ ezyhttp-server-management/src/main/java/com/tvd12/ezyhttp/server/management/monitor/SystemMonitor.java
@@ -1,51 +1,16 @@
 package com.tvd12.ezyhttp.server.management.monitor;
 
-import java.util.concurrent.atomic.AtomicLong;
+import com.tvd12.ezyfox.monitor.EzyMonitorIncludeRequestResponse;
 
-import com.tvd12.ezyfox.monitor.EzyMonitor;
-
-public class SystemMonitor extends EzyMonitor {
+public class SystemMonitor extends EzyMonitorIncludeRequestResponse {
 	
-    private final AtomicLong requestCount;
-    private EzyActionFrame requestPerSecond;
-    private EzyActionFrame reponsePerSecond;
-    
 	public static final SystemMonitor INSTANCE = new SystemMonitor(); 
 	
 	private SystemMonitor() {
-	    this.requestCount = new AtomicLong();
-	    this.requestPerSecond = new EzyActionFrameSecond();
-	    this.reponsePerSecond = new EzyActionFrameSecond();
+	    super();
 	}
 	
 	public static SystemMonitor getInstance() {
 		return INSTANCE;
 	}
-	
-	public void increaseRequestCount() {
-	    requestCount.incrementAndGet();
-	    if (requestPerSecond.isExpired()) {
-	        requestPerSecond = new EzyActionFrameSecond();
-        }
-	    requestPerSecond.addActions(1);
-	}
-	
-	public long getRequestCount() {
-	    return requestCount.get();
-	}
-	
-	public long getRequestPerSecond() {
-	    return requestPerSecond.isExpired() ? 0L : requestPerSecond.getActions();
-	}
-	
-	public void increaseResponseCount() {
-        if (reponsePerSecond.isExpired()) {
-            reponsePerSecond = new EzyActionFrameSecond();
-        }
-        reponsePerSecond.addActions(1);
-    }
-	
-	public long getResponsePerSecond() {
-        return reponsePerSecond.isExpired() ? 0L : reponsePerSecond.getActions();
-    }
 }
