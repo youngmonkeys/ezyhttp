90e556bd006aff3ac667d0dca22609fe7e34c000
Dung Ta Van <itprono3@gmail.com>
2022-05-17 22:27:37 +0700

    version 1.0.4

--- ezyhttp-server-boot/src/test/java/com/tvd12/ezyhttp/server/boot/test/controller/RedirectController.java
+++ ezyhttp-server-boot/src/test/java/com/tvd12/ezyhttp/server/boot/test/controller/RedirectController.java
@@ -2,6 +2,7 @@ package com.tvd12.ezyhttp.server.boot.test.controller;
 
 import com.tvd12.ezyhttp.server.core.annotation.Controller;
 import com.tvd12.ezyhttp.server.core.annotation.DoGet;
+import com.tvd12.ezyhttp.server.core.request.RequestArguments;
 import com.tvd12.ezyhttp.server.core.view.Redirect;
 
 @Controller("/redirect")
@@ -20,5 +21,24 @@ public class RedirectController {
     @DoGet("/to-home1")
     public Redirect toHome() {
         return Redirect.to("/?firstName=Dzung&who=Dzung");
+    }
+
+    @DoGet("/path1/hello")
+    public Redirect path1HelloGet() {
+        return Redirect.builder()
+            .uri("/redirect/path2/world")
+            .addAttribute("foo", "bar")
+            .build();
+    }
+
+    @DoGet("/path2/world")
+    public Object path2WorldGet(
+        RequestArguments arguments
+    ) {
+        System.out.println(
+            "redirect attribute: " +
+                arguments.getRedirectionAttribute("foo")
+        );
+        return "Hello World";
     }
 }
