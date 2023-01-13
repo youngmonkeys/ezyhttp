c52d0a3c03115edd6e8c6f7aeddca8255e50365d
Dung Ta Van <itprono3@gmail.com>
2023-01-13 07:45:23 +0700

    decode url

--- ezyhttp-core/src/test/java/com/tvd12/ezyhttp/core/test/io/AnywayFileLoaderTest.java
+++ ezyhttp-core/src/test/java/com/tvd12/ezyhttp/core/test/io/AnywayFileLoaderTest.java
@@ -1,11 +1,15 @@
 package com.tvd12.ezyhttp.core.test.io;
 
-import java.io.File;
-
-import org.testng.annotations.Test;
-
 import com.tvd12.ezyhttp.core.io.AnywayFileLoader;
 import com.tvd12.test.assertion.Asserts;
+import com.tvd12.test.reflect.MethodUtil;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.net.URL;
+import java.net.URLStreamHandler;
+
+import static org.mockito.Mockito.mock;
 
 public class AnywayFileLoaderTest {
 
@@ -15,7 +19,7 @@ public class AnywayFileLoaderTest {
         final String filePath = "pom.xml";
 
         // when
-        final File actual = new AnywayFileLoader().load(filePath);
+        final File actual = AnywayFileLoader.getDefault().load(filePath);
 
         // then
         Asserts.assertEquals(actual, new File("pom.xml"));
@@ -27,7 +31,7 @@ public class AnywayFileLoaderTest {
         final String filePath = "AllTests.tng.xml";
 
         // when
-        final File actual = new AnywayFileLoader().load(filePath);
+        final File actual = AnywayFileLoader.getDefault().load(filePath);
 
         // then
         Asserts.assertTrue(actual.exists());
@@ -39,7 +43,55 @@ public class AnywayFileLoaderTest {
         final String filePath = "file not found";
 
         // when
-        final File actual = new AnywayFileLoader().load(filePath);
+        final File actual = AnywayFileLoader.getDefault().load(filePath);
+
+        // then
+        Asserts.assertNull(actual);
+    }
+
+    @Test
+    public void fileNotFoundInvalidUriTest() {
+        // given
+        final String filePath = "file not found";
+
+        // when
+        final File actual = AnywayFileLoader.getDefault().load(filePath);
+
+        // then
+        Asserts.assertNull(actual);
+    }
+
+    @Test
+    public void newFileFromUrlByUriNotFoundTest() throws Exception {
+        // given
+        URL url = new File("file not found").toURI().toURL();
+        AnywayFileLoader sut = AnywayFileLoader.getDefault();
+
+        // when
+        final File actual = MethodUtil.invokeMethod(
+            "newFileFromUrl",
+            sut,
+            url
+        );
+
+        // then
+        Asserts.assertNull(actual);
+    }
+
+    @Test
+    public void newFileFromUrlByInvalidUriTest() throws Exception {
+        // given
+        URL context = new URL("http://hello");
+        URLStreamHandler handler = mock(URLStreamHandler.class);
+        URL url = new URL(context, "http://hello", handler);
+        AnywayFileLoader sut = AnywayFileLoader.getDefault();
+
+        // when
+        final File actual = MethodUtil.invokeMethod(
+            "newFileFromUrl",
+            sut,
+            url
+        );
 
         // then
         Asserts.assertNull(actual);
