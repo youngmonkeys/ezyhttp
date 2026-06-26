cd588cc31cc0c07983f5b009d2586a3d0eea600f
tvd12 <ta.van.dung@youngmonkeys.org>
2026-06-26 10:59:13 +0700

    update

--- ezyhttp-server-graphql/src/main/java/com/tvd12/ezyhttp/server/graphql/data/GraphQLDataFilter.java
+++ ezyhttp-server-graphql/src/main/java/com/tvd12/ezyhttp/server/graphql/data/GraphQLDataFilter.java
@@ -24,51 +24,7 @@ public class GraphQLDataFilter {
         Map answer = new HashMap<>();
         Deque<StackEntry> stack = new ArrayDeque<>();
         stack.push(new StackEntry(queryDefinition, data, answer));
-        while (!stack.isEmpty()) {
-            StackEntry entry = stack.pop();
-
-            GraphQLField allField = entry.field.getField(ALL_FIELDS);
-            if (allField != null && entry.data != null) {
-                Set<Map.Entry> entries = entry.data.entrySet();
-                for (Map.Entry e : entries) {
-                    Object v = e.getValue();
-                    if (v != null) {
-                        entry.output.put(e.getKey(), v);
-                    }
-                }
-            }
-
-            for (GraphQLField field : entry.field.getFields()) {
-                String fieldName = field.getName();
-                if (entry.data == null) {
-                    continue;
-                }
-                Object value = entry.data.get(fieldName);
-                if (value == null) {
-                    continue;
-                }
-                if (field.getFields().isEmpty()) {
-                    entry.output.put(fieldName, value);
-                    continue;
-                }
-                if (value instanceof Map) {
-                    Map newItem = new HashMap<>();
-                    entry.output.put(fieldName, newItem);
-                    stack.push(new StackEntry(field, (Map) value, newItem));
-                } else if (value instanceof List) {
-                    List<Map> newList = new LinkedList<>();
-                    entry.output.put(fieldName, newList);
-                    pushListItems(stack, (List) value, field, newList);
-                } else {
-                    throw new GraphQLInvalidSchemeException(
-                        EzyMapBuilder.mapBuilder()
-                            .put("schema", "invalid")
-                            .put("field", fieldName)
-                            .toMap()
-                    );
-                }
-            }
-        }
+        filterStack(stack);
         return answer;
     }
 
@@ -80,6 +36,12 @@ public class GraphQLDataFilter {
         List<Map> answer = new LinkedList<>();
         Deque<StackEntry> stack = new ArrayDeque<>();
         pushListItems(stack, dataList, queryDefinition, answer);
+        filterStack(stack);
+        return answer;
+    }
+
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    private void filterStack(Deque<StackEntry> stack) {
         while (!stack.isEmpty()) {
             StackEntry entry = stack.pop();
 
@@ -125,7 +87,6 @@ public class GraphQLDataFilter {
                 }
             }
         }
-        return answer;
     }
 
     @SuppressWarnings({"rawtypes", "unchecked"})
--- ezyhttp-server-graphql/src/test/java/com/tvd12/ezyhttp/server/graphql/test/data/GraphQLDataFilterTest.java
+++ ezyhttp-server-graphql/src/test/java/com/tvd12/ezyhttp/server/graphql/test/data/GraphQLDataFilterTest.java
@@ -5,8 +5,13 @@ import com.tvd12.ezyhttp.server.graphql.data.GraphQLField;
 import com.tvd12.test.assertion.Asserts;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
+
+import static com.tvd12.ezyfox.util.EzyMapBuilder.mapBuilder;
+import static com.tvd12.ezyhttp.server.graphql.constants.GraphQLConstants.ALL_FIELDS;
 
 public class GraphQLDataFilterTest {
 
@@ -28,5 +33,146 @@ public class GraphQLDataFilterTest {
 
         // then
         Asserts.assertEquals(result, Collections.emptyMap(), false);
+    }
+
+    @Test
+    public void filterWithAllFieldsTest() {
+        // given
+        GraphQLField queryDefinition = GraphQLField.builder()
+            .name("user")
+            .addField(
+                GraphQLField.builder()
+                    .name(ALL_FIELDS)
+                    .build()
+            )
+            .build();
+        Map data = mapBuilder()
+            .put("id", 1)
+            .put("name", "Dzung")
+            .put("nullable", null)
+            .toMap();
+
+        // when
+        Map result = instance.filter(data, queryDefinition);
+
+        // then
+        Asserts.assertEquals(
+            result,
+            mapBuilder()
+                .put("id", 1)
+                .put("name", "Dzung")
+                .toMap(),
+            false
+        );
+    }
+
+    @Test
+    public void filterWithAllFieldsAndNestedOverrideTest() {
+        // given
+        GraphQLField queryDefinition = GraphQLField.builder()
+            .name("user")
+            .addField(
+                GraphQLField.builder()
+                    .name(ALL_FIELDS)
+                    .build()
+            )
+            .addField(
+                GraphQLField.builder()
+                    .name("friends")
+                    .addField(
+                        GraphQLField.builder()
+                            .name("name")
+                            .build()
+                    )
+                    .build()
+            )
+            .build();
+        Map data = mapBuilder()
+            .put("id", 1)
+            .put(
+                "friends",
+                Arrays.asList(
+                    mapBuilder()
+                        .put("id", 2)
+                        .put("name", "Foo")
+                        .toMap(),
+                    mapBuilder()
+                        .put("id", 3)
+                        .put("name", "Bar")
+                        .toMap()
+                )
+            )
+            .toMap();
+
+        // when
+        Map result = instance.filter(data, queryDefinition);
+
+        // then
+        Asserts.assertEquals(
+            result,
+            mapBuilder()
+                .put("id", 1)
+                .put(
+                    "friends",
+                    Arrays.asList(
+                        mapBuilder()
+                            .put("name", "Foo")
+                            .toMap(),
+                        mapBuilder()
+                            .put("name", "Bar")
+                            .toMap()
+                    )
+                )
+                .toMap(),
+            false
+        );
+    }
+
+    @Test
+    public void filterListWithAllFieldsTest() {
+        // given
+        GraphQLField queryDefinition = GraphQLField.builder()
+            .name("friends")
+            .addField(
+                GraphQLField.builder()
+                    .name(ALL_FIELDS)
+                    .build()
+            )
+            .build();
+
+        // when
+        Object result = instance.filterList(
+            Arrays.asList(
+                new HashMap<>(
+                    mapBuilder()
+                        .put("id", 1)
+                        .put("name", "Foo")
+                        .toMap()
+                ),
+                new HashMap<>(
+                    mapBuilder()
+                        .put("id", 2)
+                        .put("name", "Bar")
+                        .toMap()
+                )
+            ),
+            queryDefinition
+        );
+
+        // then
+        Asserts.assertEquals(
+            result,
+            Arrays.asList(
+                mapBuilder()
+                    .put("id", 1)
+                    .put("name", "Foo")
+                    .toMap(),
+                mapBuilder()
+                    .put("id", 2)
+                    .put("name", "Bar")
+                    .toMap()
+            ),
+            false
+        );
     }
 }
